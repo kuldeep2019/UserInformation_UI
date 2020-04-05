4be6ecf155101458165d7eeeaa1cf747f8898184
icici-xpress <milind@cateina.com>
2020-04-05 16:43:14 +0530

    google user info component changed

--- src/app/pages/google-user-info-form/google-user-info-form.component.ts
+++ src/app/pages/google-user-info-form/google-user-info-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-google-user-info-form',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./google-user-info-form.component.css']
 })
 export class GoogleUserInfoFormComponent implements OnInit {
-
-  constructor() { }
+  fullName;
+  email;
+  photo;
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      this.fullName = params['fullName'];
+      this.email = params['email'];
+      this.photo = params['photo'];
+      console.log("params: ",this.fullName,this.email,this.photo)
+  });
   }
 
 }
